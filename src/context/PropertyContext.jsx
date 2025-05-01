154fc8c7121127cea5de09d3298ee1b126e76a7c
Vijay Singh Parihar <121249393+vijaysingh60@users.noreply.github.com>
2025-05-01 12:32:12 +0530

    Update PropertyContext.jsx

--- src/context/PropertyContext.jsx
+++ src/context/PropertyContext.jsx
@@ -1,7 +1,8 @@
 import { createContext, useState, useContext, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = import.meta.env.API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
+
 
 
 const PropertyContext = createContext();
