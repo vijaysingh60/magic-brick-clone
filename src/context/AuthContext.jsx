4dcaee018268e0128ddfe50a28c6ea118f6c69e4
Vijay Singh Parihar <vsp.krashi@gmail.com>
2025-05-01 13:22:20 +0530

    scroll problem

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useState, useContext, useEffect } from 'react';
 import axios from 'axios';
 
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const AuthContext = createContext();
 
